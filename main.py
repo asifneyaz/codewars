a8cfb8b41a3764058daf63d07ee4de538e23fdc2
[asifneyaz] <asifneyaz@gmail.com>
2021-06-30 22:47:32 -0500

    changed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#heThis is a sample Python script.
+#hello all This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
